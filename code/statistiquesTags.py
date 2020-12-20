048a285f2a25dad4b29654b379113d3c63fb67a2
Louis Martin <Louis.martin@mesnouvelleseconomiques.com>
2020-12-19 21:53:13 -0600

    nouvelle video

--- code/statistiquesTags.py
+++ code/statistiquesTags.py
@@ -4,7 +4,7 @@ import collections
 print("OK")
 theTags=[]
 
-with open('/data/sources.json') as json_file:
+with open('../sources.json') as json_file:
     data = json.load(json_file)
     for p in data:
         theTags = theTags + p['tags']
